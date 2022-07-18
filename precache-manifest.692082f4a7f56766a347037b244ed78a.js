9ff937beaa991380ab5a50c8d949fef61df82fda
desarrolladorp09 <codigop09@gmail.com>
2022-07-18 09:23:59 -0500

    Updates

---
+++ precache-manifest.692082f4a7f56766a347037b244ed78a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "414beb4730ca603c130b25671ab50926",
+    "url": "/HappyAnniversary/index.html"
+  },
+  {
+    "revision": "89f633e110130ef413b4",
+    "url": "/HappyAnniversary/static/css/main.f1f73786.chunk.css"
+  },
+  {
+    "revision": "2a8ce47eb7bfc95b1c0d",
+    "url": "/HappyAnniversary/static/js/2.78186614.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HappyAnniversary/static/js/2.78186614.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89f633e110130ef413b4",
+    "url": "/HappyAnniversary/static/js/main.c4c25410.chunk.js"
+  },
+  {
+    "revision": "3fef049a3136f22c9c34",
+    "url": "/HappyAnniversary/static/js/runtime-main.836ffc1d.js"
+  },
+  {
+    "revision": "58cc117001de0d11fef59a00943ddd81",
+    "url": "/HappyAnniversary/static/media/Fredoka.58cc1170.ttf"
+  }
+]);
